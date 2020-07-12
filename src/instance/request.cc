dd2aafd32d1962379f3bb3b67ee404af17b1bceb
jingbing.du <dujingbing_csu@163.com>
2020-07-12 14:21:56 +0800

    add system function of 'HttpResponse::system_info'.

--- src/instance/request.cc
+++ src/instance/request.cc
@@ -2,6 +2,32 @@
 
 void instance::HttpRequest::system_info(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
+    // prepare v8 context.
+    v8::Isolate *isolate = args.GetIsolate();
+    v8::HandleScope handle_scope(isolate);
+    v8::Context::Scope context_scope(isolate->GetCurrentContext());
+    v8::TryCatch try_catch(isolate);
+
+    // Get execution context.
+    v8::Local<v8::External> ctx_data = v8::Local<v8::External>::Cast(args.Data());
+    instance::ExecuteContext *ctx = static_cast<instance::ExecuteContext *>(ctx_data->Value());
+
+    // Get gRPC objects.
+    protos::RuntimeRequest *request = ctx->request();
+
+    // Build system info object.
+    v8::Local<v8::Object> info = v8::Object::New(isolate);
+    info->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "name"),
+              instance::Util::v8_str(isolate, request->call().function().c_str()));
+    info->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "version"),
+              instance::Util::v8_str(isolate, request->call().version().c_str()));
+    info->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "group"),
+              instance::Util::v8_str(isolate, request->call().group().c_str()));
+    info->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "unit"),
+              instance::Util::v8_str(isolate, request->call().unit().c_str()));
+    info->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "timeout"),
+              v8::Integer::New(isolate, request->call().timeout()));
+    args.GetReturnValue().Set(info);
 }
 
 void instance::HttpRequest::content_type(const v8::FunctionCallbackInfo<v8::Value> &args)

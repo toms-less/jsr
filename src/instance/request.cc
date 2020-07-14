52da60d4c17b7e0cba6c05fe3ca5768501b6254e
jingbing.du <dujingbing_csu@163.com>
2020-07-14 19:50:54 +0800

    add system function of 'HttpRequest'.

--- src/instance/request.cc
+++ src/instance/request.cc
@@ -261,6 +261,54 @@ void instance::HttpRequest::cookie(const v8::FunctionCallbackInfo<v8::Value> &ar
 
 void instance::HttpRequest::cookies(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
+    // prepare v8 context.
+    v8::Isolate *isolate = args.GetIsolate();
+    v8::HandleScope handle_scope(isolate);
+    v8::Context::Scope context_scope(isolate->GetCurrentContext());
+    v8::TryCatch try_catch(isolate);
+
+    const int args_length = args.Length();
+    if (args_length != 0)
+    {
+        const char *msg = "'cookies' function should have no parameter.";
+        v8::Local<v8::Object> error = instance::Util::error(isolate, "user", msg, msg);
+        isolate->ThrowException(error);
+        return;
+    }
+
+    // Get execution context.
+    v8::Local<v8::External> ctx_data = v8::Local<v8::External>::Cast(args.Data());
+    instance::ExecuteContext *ctx = static_cast<instance::ExecuteContext *>(ctx_data->Value());
+
+    // Get gRPC objects.
+    protos::RuntimeRequest *request = ctx->request();
+    const google::protobuf::RepeatedPtrField<protos::Cookie> &cookies = request->call().cookies();
+    v8::Local<v8::Array> v8_cookies = v8::Array::New(isolate);
+    for (int i = 0; i < cookies.size(); i++)
+    {
+        const protos::Cookie &cookie = cookies[i];
+        // Build cookie object.
+        v8::Local<v8::Object> v8_cookie = v8::Object::New(isolate);
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "name"),
+                       instance::Util::v8_str(isolate, cookie.name().c_str()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "value"),
+                       instance::Util::v8_str(isolate, cookie.value().c_str()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "domain"),
+                       instance::Util::v8_str(isolate, cookie.domain().c_str()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "path"),
+                       instance::Util::v8_str(isolate, cookie.path().c_str()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "expires"),
+                       v8::Integer::New(isolate, cookie.expires()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "maxAge"),
+                       v8::Integer::New(isolate, cookie.maxage()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "secure"),
+                       v8::Boolean::New(isolate, cookie.secure()));
+        v8_cookie->Set(isolate->GetCurrentContext(), instance::Util::v8_str(isolate, "httpOnly"),
+                       v8::Boolean::New(isolate, cookie.httponly()));
+
+        v8_cookies->Set(isolate->GetCurrentContext(), i, v8_cookie);
+    }
+    args.GetReturnValue().Set(v8_cookies);
 }
 
 void instance::HttpRequest::parameter(const v8::FunctionCallbackInfo<v8::Value> &args)
